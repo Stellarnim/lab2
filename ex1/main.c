70717914a69a223b261f7ae94361a260930ed654
stm <guym3913@naver.com>
2023-10-29 12:01:36 +0900

    ex1

---
+++ ex1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "static.h"
+
+int main(){
+int a = 10;
+int b = 2;
+
+printf("add : %d\n", add(a,b));
+printf("sub : %d\n", sub(a,b));
+printf("mul : %d\n", mul(a,b));
+printf("div : %d\n", div(a,b));
+
+return 0;
+}
+
